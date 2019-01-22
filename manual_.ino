6243fcbd7c2072be93f2da88130bb32697c3807d
Claire <b6008661@my.shu.ac.uk>
2019-01-22 15:06:03 +0000

    Task 2

--- manual_.ino
+++ manual_.ino
@@ -23,12 +23,38 @@ unsigned int sensor_values[6];
 char currentSetting; //determines which mode we are currently in ie. manual/autonomous 
 bool wall;
 String val;
+int maxSensorValue[6];
 
 /*-----setup-----*/
 void setup() 
 {
     Serial.begin(9600);
     val = Serial.read();
+
+    //initialise reflectnace sensors module 
+    sensors.init();
+    //automatic sensor calibration by rotating in place to sweep the sensors - similar 
+    //to the Line Follower example
+    for(int i = 0; i < 80; i++)
+    {
+      if((i>10 && i<=30) || (i>50 && i <=70))
+      {
+        motors.setSpeeds(-NORMAL_SPEED, NORMAL_SPEED);
+      }
+      else 
+      {
+        motors.setSpeeds(NORMAL_SPEED, -NORMAL_SPEED);
+      }
+    }
+    sensors.calibrate();
+    delay(20);
+    for(int i = 0; i < 6; i++)
+    {
+      maxSensorValue[i] = sensors.calibrateMaximumOn[i];
+    }
+
+    motors.setSpeeds(0,0);
+
     while(val != "z")
     {
       val = Serial.read();
@@ -58,33 +84,53 @@ void moveManually() {
   if(Serial.available() > 0) {
     String val = Serial.read();
 
-    if(val == "f") {
-      motors.setSpeeds(MAX_SPEED, MAX_SPEED);
-      delay(500);
-      motors.setSpeeds(0,0);
+    switch(val) {
+
+        case "f":
+          motors.setSpeeds(NORMAL_SPEED, NORMAL_SPEED);
+          delay(2000);
+          motors.setSpeeds(0,0);
+          break;
+
+        case "b":
+          motors.setSpeeds(-NORMAL_SPEED, -NORMAL_SPEED);
+          delay(2000);
+          motors.setSpeeds(0,0);
+          break;
+          
+        case "l":
+          motors.setSpeeds(NORMAL_SPEED, -NORMAL_SPEED);
+          delay(2000);
+          motors.setSpeeds(0,0);
+          break;
+          
+        case "r": 
+          motors.setSpeeds(-NORMAL_SPEED, NORMAL_SPEED);
+          delay(2000);
+          motors.setSpeeds(0,0);
+          break;
+          
+        case "s":
+          motors.setSpeeds(0,0);
+          break;
+          
+        case "LeRo":
+          motors.setSpeeds(0,0);
+          break;
+          
+        case "RiRo":
+          motors.setSpeeds(0,0);
+          break;
+          
+        case "C":
+          currentSetting = 'a';
+          break;
+          
+        case "E":
+          motors.setSpeeds(0,0);
+          break; 
     }
 
-    if(val == "b") {
-      motors.setSpeeds(-MAX_SPEED, -MAX_SPEED);
-      delay(500);
-      motors.setSpeeds(0,0);
-    }
-
-    if(val == "l") {
-      motors.setSpeeds(MAX_SPEED, -MAX_SPEED);
-      delay(500);
-      motors.setSpeeds(0,0);
-    }
-
-    if(val == "r") {
-      motors.setSpeeds(-MAX_SPEED, MAX_SPEED);
-      delay(500);
-      motors.setSpeeds(0,0);
-    }
-    if(val == "s") {
-      motors.setSpeeds(0,0);
-      delay(500);
-    }
     if(val == "LeRo") {
       //give room a number 
       //tell gui number and left 
@@ -99,8 +145,59 @@ void moveManually() {
       //scan room 
       //if object is detected, tell gui 
     }
-    if(val == "C") {
-      //turn back to autonomous 
+  }
+}
+
+void moveAutonomously() {
+  //move along the corridor keeping within the black lines until it reaches a corner
+  val = Serial.read();
+  sensors.read(sensor_values);
+
+  if(val = "s")
+  {
+    motors.setSpeeds(0,0);
+  }
+  else {
+    if(val = "LeRo")
+    {
+      //go to manual mode 
+    }
+    else {
+      if(val = "RiRo")
+      {
+        //go to manual mode
+      }
+      else {
+        if(sensor_values[3] > maxSensorValue[3])
+        {
+          motors.setSpeed(0,0);
+          currentSetting = 'm';
+        }
+        else {
+          if(sensor_values[5] >= maxSensorValue[5])
+          {
+            motors.setSpeeds(-REVERSE_SPEED, _REVERSE_SPEED);
+            delay(REVERSE_DURATION);
+            motor.setSpeeds(TURN_SPEED, -TURN_SPEED);
+            delay(TURN_DURATION);
+            motors.setSpeeds(NORMAL_SPEED, NORMAL_SPEED);
+          }
+          else {
+            if(sensor_values[0] >= maxSensorValue[0])
+            {
+              motors.setSpeeds(-REVERSE_SPEED, _REVERSE_SPEED);
+              delay(REVERSE_DURATION);
+              motor.setSpeeds(-TURN_SPEED, TURN_SPEED);
+              delay(TURN_DURATION);
+              motors.setSpeeds(NORMAL_SPEED, NORMAL_SPEED);
+            }
+            else {
+              motors.setSpeeds(NORMAL_SPEED, NORMAL_SPEED);
+            }
+          }
+        }
+      }
     }
   }
+  
 }
